8101b41dee335d05f93718c4d177b2919d0ec032
Oleg Paramey <olegparamey4@gmail.com>
2025-02-04 15:42:51 +0100

    Show cards with favorite quotes in it below current card with quote

--- random-quotes/quotes.js
+++ random-quotes/quotes.js
@@ -2,58 +2,72 @@ const quotes = [
 	{
 		quote: "The only way to do great work is to love what you do",
 		author: "Steve Jobs",
+		isFavorite: false,
 	},
 	{
 		quote: "Innovation distinguishes between a leader and a follower",
 		author: "Steve Jobs",
+		isFavorite: false,
 	},
 	{
 		quote: "Your time is limited, so don't waste it living someone else's life",
 		author: "Steve Jobs",
+		isFavorite: false,
 	},
 	{
 		quote: "Stay hungry, stay foolish",
 		author: "Steve Jobs",
+		isFavorite: false,
 	},
 	{
 		quote: "Democracy is the worst form of government, except for all the others that have been tried",
 		author: "Winston Churchill",
+		isFavorite: false,
 	},
 	{
 		quote: "Injustice anywhere is a threat to justice everywhere",
 		author: "Martin Luther King Jr.",
+		isFavorite: false,
 	},
 	{
 		quote: "Ask not what your country can do for you – ask what you can do for your country",
 		author: "John F. Kennedy",
+		isFavorite: false,
 	},
 	{
 		quote: "The only thing we have to fear is fear itself",
 		author: "Franklin D. Roosevelt",
+		isFavorite: false,
 	},
 	{
 		quote: "Politics is war without bloodshed while war is politics with bloodshed",
 		author: "Mao Zedong",
+		isFavorite: false,
 	},
 	{
 		quote: "Freedom is never more than one generation away from extinction",
 		author: "Ronald Reagan",
+		isFavorite: false,
 	},
 	{
 		quote: "The ballot is stronger than the bullet",
 		author: "Abraham Lincoln",
+		isFavorite: false,
 	},
 	{
 		quote: "Power tends to corrupt, and absolute power corrupts absolutely",
 		author: "Lord Acton",
+		isFavorite: false,
 	},
 	{
 		quote: "To walk safely through the maze of human life, one needs the light of wisdom and the guidance of virtue",
 		author: "Napoleon Bonaparte",
+		isFavorite: false,
 	},
 	{
 		quote: "A leader is a dealer in hope",
 		author: "Napoleon Bonaparte",
+		isFavorite: false,
 	},
 ];
 
--- random-quotes/script.js
+++ random-quotes/script.js
@@ -3,17 +3,47 @@ import quotes from "./quotes.js";
 const quoteElement = document.getElementById("quote");
 const generateButton = document.getElementById("generate-btn");
 const quoteAuthorElement = document.getElementById("quote-author");
-let lastIndex = -1;
-let randomIndex = -1;
+const toggleFavoriteButton = document.getElementById("toggle-favorite-btn");
+const favoritesContainer = document.getElementById("favorites-container");
+
+let currentQuoteIndex = null;
 
 function generateRandomQuote() {
-	while (lastIndex == randomIndex) {
+	let randomIndex = null;
+	do {
 		randomIndex = Math.floor(Math.random() * quotes.length);
-	}
-	lastIndex = randomIndex;
+	} while (currentQuoteIndex == randomIndex);
+	currentQuoteIndex = randomIndex;
 	const randomQuote = quotes[randomIndex];
 	quoteElement.textContent = randomQuote.quote;
 	quoteAuthorElement.textContent = randomQuote.author;
+	toggleFavoriteButton.textContent = randomQuote.isFavorite ? "Remove from favorites" : "Add to favorites";
+	toggleFavoriteButton.style.display = "inline-block";
+}
+
+function toggleFavorite() {
+	if (currentQuoteIndex === null || !quotes[currentQuoteIndex]) return;
+	const currentQuote = quotes[currentQuoteIndex];
+	currentQuote.isFavorite = !currentQuote.isFavorite;
+	toggleFavoriteButton.textContent = currentQuote.isFavorite ? "Remove from favorites" : "Add to favorites";
+
+	if (currentQuote.isFavorite) {
+		const favoriteCard = document.createElement("div");
+		favoriteCard.classList.add("favorite-card");
+		favoriteCard.innerHTML = `
+		<p class="quote">${currentQuote.quote}</p>
+		<p id="quote-author"">${currentQuote.author}</p>
+		`;
+		favoritesContainer.appendChild(favoriteCard);
+	} else {
+		const favoriteCards = document.querySelectorAll(".favorite-card");
+		favoriteCards.forEach((card) => {
+			if (card.textContent.includes(currentQuote.quote)) {
+				card.remove();
+			}
+		});
+	}
 }
 
 generateButton.addEventListener("click", generateRandomQuote);
+toggleFavoriteButton.addEventListener("click", toggleFavorite);
