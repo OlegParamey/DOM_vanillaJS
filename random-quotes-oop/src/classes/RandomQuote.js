fd719ccf8bd084638cd69006f0a3b4ae657bb62c
Oleg Paramey <olegparamey4@gmail.com>
2025-02-17 17:40:48 +0100

    Get random quotes via public API service

--- random-quotes-oop/src/classes/RandomQuote.js
+++ random-quotes-oop/src/classes/RandomQuote.js
@@ -9,5 +9,20 @@ class RandomQuote {
     const { id, text, author } = quotes[randomIndex];
     return new Quote(id, text, author)
   }
+
+  static getRandomQuoteViaAPI() {
+    const url = "https://quoteslate.vercel.app/api/quotes/random";
+    const options = { headers: { "Content-Type": "application/json" } };
+
+    return fetch(url, options)
+      .then((response) => response.json())
+      .then((data) => {
+        const id = data.id;
+        const text = data.quote;
+        const author = data.author;
+        return new Quote(id, text, author)
+      })
+      .catch((error) => console.error(error));
+  }
 }
 export default RandomQuote;
