186f331fda86c17516e01e04445462e8a3f3f661
Oleg Paramey <olegparamey4@gmail.com>
2025-02-21 19:15:31 +0100

    Add README.md and config in the client app

---
+++ random-quotes-oop/client/config.sample.js
@@ -0,0 +1,5 @@
+const config = {
+	PUBLIC_API_URL: "Address of the public API server", //Here was used this one: https://quoteslate.vercel.app
+	API_URL: "Address of our own API server", // For example: http://localhost:3000
+};
+export default config;
